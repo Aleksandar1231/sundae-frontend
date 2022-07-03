bad263aa296d2daf7968af33e180f4abebad021f
rondadefidon <flyfreshfits@gmail.com>
2022-07-03 13:07:06 -0400

    nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -110,9 +110,9 @@ const Nav = () => {
               <Link color="textPrimary" to="/nodes" className='nav-link'>
                 Nodes
               </Link>
-             {/* <Link color="textPrimary" to="/regulations" className={classes.link}>
+             <Link color="textPrimary" to="/regulations" className='nav-link'>
                 Regulations
-              </Link> */}
+              </Link>
               <a href="https://froyo.farm/#/avax" target="_blank" className='nav-link'>
                 Vaults
               </a>
